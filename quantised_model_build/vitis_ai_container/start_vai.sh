edcc11772d92de8043565e1c44d66d97c483d037
Changhong <15284244183@163.com>
2025-09-26 10:41:18 +0100

    add sh files

---
+++ quantised_model_build/vitis_ai_container/start_vai.sh
@@ -0,0 +1,2 @@
+cd /home/changhong/prj/Emerald_Video_Denoise_Acc/vai_unet/2_vitis_ai_unet/5_vai_container
+./docker_run.sh xilinx/vitis-ai-cpu:1.4.1.978
---
+++ quantised_model_build/vitis_ai_container/start_vai_25gpu.sh
@@ -0,0 +1,2 @@
+cd /home/changhong/prj/Emerald_Video_Denoise_Acc/vai_unet/2_vitis_ai_unet/5_vai_container
+./docker_run.sh joocoo/vitis-ai-gpu:latest
